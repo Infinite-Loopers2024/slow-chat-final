811d1d41ed1028f10f41a5fd144deee7e2be8556
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-11 14:04:47 +0100

    header styling
    Co-authored-by: Ali <lali.mohseni05@yahoo.se>
    Co-authored-by: Love <love.bellinder@gmail.com>
    Co-authored-by: Adrienn <apozsgai97@gmail.com>

--- app/(chat-rum)/layout.tsx
+++ app/(chat-rum)/layout.tsx
@@ -4,16 +4,22 @@ export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <div>
-        <nav>
-          <Link href=".">logo</Link>
-          <ul>
-            <li>
-              <Link href="\chat\">Chat</Link>
-            </li>
-            <li>
-              <Link href="\statistics\">Statistics</Link>
-            </li>
-          </ul>
+        <nav className="navbar bg-base-100">
+          <div className="flex-1">
+            <Link className="btn btn-ghost text-xl" href=".">
+              logo
+            </Link>
+          </div>
+          <div className="flex-none">
+            <ul className="menu menu-horizontal px-1">
+              <li className="px-2">
+                <Link href="\chat\">Chat</Link>
+              </li>
+              <li>
+                <Link href="\statistics\">Statistics</Link>
+              </li>
+            </ul>
+          </div>
         </nav>
       </div>
       <div>{children}</div>
