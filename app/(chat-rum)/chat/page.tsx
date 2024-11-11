25323b3609272af555604854158d5ae42ed0620b
defycab <love.bellinder@gmail.com>
2024-11-11 13:50:41 +0100

    add daisy ui and minor styling
    Co-authored-by: Mårten Söderlind <marten.soderlind@appliedtechnology.se>
    Co-authored-by: Love <ali.mohseni05@yahoo.se>
    Co-authored-by: Adrienn <apozsgai97@gmail.com>

--- app/(chat-rum)/chat/page.tsx
+++ app/(chat-rum)/chat/page.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import "./styles.css"
 
 export default function ChatPage() {
   return (
-    <div>
+    <div className="container mx-auto">
       <div id="message-placeholder"></div>
       <div id="counter"></div>
       <button>Fetch messages</button>
       <form action="submit">
-        <input type="text" id="message" />
+        <input className="input input-bordered input-warning w-full max-w-xs" placeholder="type here" type="text" id="message" />
         <button>Send message</button>
       </form>
     </div>
