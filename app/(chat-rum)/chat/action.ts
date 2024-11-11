16947b7402ef40dbaa06879cd4b1eaf6901f8e6d
defycab <love.bellinder@gmail.com>
2024-11-11 16:12:10 +0100

    add useState and handlesubmit
    Co-authored-by: Mårten Söderlind <marten.soderlind@appliedtechnology.se>
    Co-authored-by: Love <ali.mohseni05@yahoo.se>
    Co-authored-by: Adrienn <apozsgai97@gmail.com>

--- app/(chat-rum)/chat/action.ts
+++ app/(chat-rum)/chat/action.ts
@@ -1,6 +1,6 @@
-"use server"
+"use server";
 
-export async function createMessage(formData: FormData){
- const rawMessage = {message: formData.get("message")}
- console.log(rawMessage.message);
+export async function createMessage(formData: FormData) {
+  const { message } = { message: formData.get("message") };
+  console.log(message);
 }
--- app/(chat-rum)/chat/page.tsx
+++ app/(chat-rum)/chat/page.tsx
@@ -1,6 +1,18 @@
+"use client";
+
+import { useState } from "react";
 import { createMessage } from "./action";
 
 export default function ChatPage() {
+  const [input, setInput] = useState("");
+  const [message, setMessage] = useState<string[]>([]);
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    setMessage((prevmessage) => [...prevmessage, input]);
+    console.log(message)
+  };
+
   return (
     <main className="container mx-auto pb-6 p-2 rounded-md bg-base-200 mt-2 h-[85lvh]">
       <div
@@ -13,14 +25,18 @@ export default function ChatPage() {
         </div>
         <button className="btn btn-secondary m-1 w-52">Fetch messages</button>
       </div>
-      <form className="flex items-center justify-end" action={createMessage}>
+      <form className="flex items-center justify-end" onSubmit={handleSubmit}>
         <input
           className="input input-bordered input-warning w-full max-w-xs m-3"
           placeholder="type here"
           type="text"
           id="message"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
         />
-        <button className="btn btn-secondary m-1 w-52" type="submit">Send message</button>
+        <button className="btn btn-secondary m-1 w-52" type="submit">
+          Send message
+        </button>
       </form>
     </main>
   );
