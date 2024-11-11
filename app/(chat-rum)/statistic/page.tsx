b638ef0c1e3f4b44e244d4b0d8460662b9b1f190
Ali Mohseni <ali.mohseni@appliedtechnology.se>
2024-11-11 13:26:17 +0100

    added statistics page
    Co-authored-by: Mårten Söderlind <marten.soderlind@appliedtechnology.se>
    Co-authored-by: Love <love.bellinder@gmail.com>
    Co-authored-by: Ali <ali.mohseni05@yahoo.se>

---
+++ app/(chat-rum)/statistic/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function StatisticsPage() {
+  return (
+    <>
+      <div className="">total messages</div>
+      <div>Number of new messages/fetch + coldwon</div>
+      <div>Number of new messages/fetch</div>
+    </>
+  );
+}
