fc34eaa43023367cd5d8113499ee7c32d09b53c1
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-11 14:52:52 +0100

    styling buttons
    Co-authored-by: Ali <lali.mohseni05@yahoo.se>
    Co-authored-by: Love <love.bellinder@gmail.com>
    Co-authored-by: Adrienn <apozsgai97@gmail.com>

--- app/(chat-rum)/chat/page.tsx
+++ app/(chat-rum)/chat/page.tsx
@@ -7,16 +7,20 @@ export default function ChatPage() {
         id="message-placeholder"
         className="h-4/6 bg-base-100 rounded-md m-2"
       ></div>
-      <div id="counter"></div>
-      <button className="btn m-1">Fetch messages</button>
-      <form action="submit">
+      <div className="flex items-center justify-end">
+        <div id="counter" className="mr-20">
+          token:3
+        </div>
+        <button className="btn btn-secondary m-1 w-52">Fetch messages</button>
+      </div>
+      <form className="flex items-center justify-end" action="submit">
         <input
-          className="input input-bordered input-warning w-full max-w-xs"
+          className="input input-bordered input-warning w-full max-w-xs m-3"
           placeholder="type here"
           type="text"
           id="message"
         />
-        <button className="btn m-1">Send message</button>
+        <button className="btn btn-secondary m-1 w-52">Send message</button>
       </form>
     </main>
   );
