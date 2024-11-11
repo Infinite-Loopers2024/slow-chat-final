b4ce703e605179d9893b2ed32824490b71e68d59
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-11 14:55:22 +0100

    padding bot
    Co-authored-by: Ali <lali.mohseni05@yahoo.se>
    Co-authored-by: Love <love.bellinder@gmail.com>
    Co-authored-by: Adrienn <apozsgai97@gmail.com>

--- app/(chat-rum)/chat/page.tsx
+++ app/(chat-rum)/chat/page.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function ChatPage() {
   return (
-    <main className="container mx-auto p-2 rounded-md bg-base-200 mt-2 h-[85lvh]">
+    <main className="container mx-auto pb-6 p-2 rounded-md bg-base-200 mt-2 h-[85lvh]">
       <div
         id="message-placeholder"
-        className="h-4/6 bg-base-100 rounded-md m-2"
+        className="h-5/6 bg-base-100 rounded-md m-2"
       ></div>
       <div className="flex items-center justify-end">
         <div id="counter" className="mr-20">
