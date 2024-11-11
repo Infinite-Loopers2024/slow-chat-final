41681950ba9c7a75e4ec3071ddcde1073cca53c3
Ali Mohseni <ali.mohseni@appliedtechnology.se>
2024-11-11 14:43:03 +0100

    justify content
    Co-authored-by: Mårten Söderlind <marten.soderlind@appliedtechnology.se>
    Co-authored-by: Love <love.bellinder@gmail.com>
    Co-authored-by: Ali <ali.mohseni05@yahoo.se>

--- app/(chat-rum)/chat/page.tsx
+++ app/(chat-rum)/chat/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
-
 export default function ChatPage() {
   return (
-    <main className="container mx-auto p-2 rounded-md bg-base-200 mt-2">
-      <div id="message-placeholder"></div>
+    <main className="container mx-auto p-2 rounded-md bg-base-200 mt-2 h-[85lvh]">
+      <div
+        id="message-placeholder"
+        className="h-4/6 bg-base-100 rounded-md m-2"
+      ></div>
       <div id="counter"></div>
       <button className="btn m-1">Fetch messages</button>
       <form action="submit">
