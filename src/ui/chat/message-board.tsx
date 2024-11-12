3cde2213c4d5ec220cdd86f51e635e7e0044679a
Ali Mohseni <ali.mohseni@appliedtechnology.se>
2024-11-12 11:11:54 +0100

    added overflow to msg-board
    Co-authored-by: Mårten Söderlind <marten.soderlind@appliedtechnology.se>
    Co-authored-by: Love <love.bellinder@gmail.com>
    Co-authored-by: Ali <ali.mohseni05@yahoo.se>

--- src/ui/chat/message-board.tsx
+++ src/ui/chat/message-board.tsx
@@ -3,7 +3,10 @@ import Message from "./message";
 
 export default function MessageBoard({ messages }: { messages: string[] }) {
   return (
-    <div id="message-placeholder" className="h-5/6 bg-base-100 rounded-md m-2">
+    <div
+      id="message-placeholder"
+      className="h-5/6 bg-base-100 rounded-md m-2 overflow-auto flex flex-col-reverse"
+    >
       {messages.map((message, index) => (
         <Message key={index} message={message} />
       ))}
