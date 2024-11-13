4bb420b44d7c5c1d512d0fa6cc9736b15aa6cdaf
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-13 08:40:23 +0100

    stying of login Co-authored-by: Ali <lali.mohseni05@yahoo.se> Co-authored-by: Love <love.bellinder@gmail.com> Co-authored-by: Ali <ali.mohseni05@yahoo.se>

--- src/features/login/login-form.tsx
+++ src/features/login/login-form.tsx
@@ -5,9 +5,9 @@ export default function LoginForm() {
       className="container mx-auto my-52 flex flex-col bg-base-200 p-3 rounded-md"
     >
       <label htmlFor="user-name">Username</label>
-      <input type="text" name="" id="user-name" required />
+      <input type="text" name="" id="user-name" required className="input" />
       <label htmlFor="password">Password</label>
-      <input type="text" name="" id="password" required />
+      <input type="text" name="" id="password" required className="input" />
       <div>
         <button className="btn my-2 bg-base-300">Join</button>
       </div>
