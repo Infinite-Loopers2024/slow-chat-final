55e0f1d3f6653334325e75158c9ff66f37ae2da5
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-11-13 15:49:53 +0100

    refactoring service actions
    Co-authored-by: Mårten Söderlind <marten.soderlind@appliedtechnology.se>
    Co-authored-by: Love <love.bellinder@gmail.com>
    Co-authored-by: Ali <ali.mohseni05@yahoo.se>

--- src/features/chat/actions.ts
+++ src/features/chat/actions.ts
@@ -13,9 +13,9 @@ export async function addMessage(formData: FormData) {
 }
 
 export async function revalidateMessages() {
-  const currentUserId = "7539a68c-9373-42c1-8ec1-5b6ea9e77016";
-  const newTime = await chatFeature.service.updateFetchTime(currentUserId);
-  console.log(newTime);
+  const currentUserId = "9e272678-02e9-445c-a77a-82122fada7da";
+  await chatFeature.service.updateFetchTime(currentUserId);
+
   const tokens = await chatFeature.service.getAllUserTokens(currentUserId);
   if (!tokens) {
     return;
@@ -23,37 +23,6 @@ export async function revalidateMessages() {
   await chatFeature.service.reduceUserToken(currentUserId);
   await getFetchedMessages(new Date().toISOString());
   revalidatePath("/chat");
-}
-
-export async function calculateTimeDifference(currentDate: Date) {
-  const approvedDates = [];
-  const cooldownDates = [];
-
-  const messages = await chatFeature.service.getAllMessages();
-  const currentDateString = currentDate.toISOString();
-  const dates = messages.map((message) => {
-    return message.timeStamp;
-  });
-
-  const datesToCheck = [];
-
-  for (let i = 0; i < dates.length; i++) {
-    const date = new Date(dates[i]);
-    date.setHours(date.getHours() + 1);
-    const updatedDate = date.toISOString();
-    datesToCheck.push(updatedDate);
-  }
-
-  for (let j = 0; j < datesToCheck.length; j++) {
-    if (datesToCheck[j] < currentDateString) {
-      approvedDates.push(datesToCheck[j]);
-    } else {
-      cooldownDates.push(datesToCheck[j]);
-    }
-  }
-
-  console.log(approvedDates);
-  console.log(cooldownDates);
 }
 
 export async function getFetchedMessages(fetchedDate: string) {
--- src/features/chat/components/chat.tsx
+++ src/features/chat/components/chat.tsx
@@ -4,10 +4,12 @@ import { getFetchedMessages } from "../actions";
 import { chatFeature } from "../instance";
 
 export async function ChatPage() {
-  const fetchedDate = await chatFeature.service.getFetchedDate(
-    "7539a68c-9373-42c1-8ec1-5b6ea9e77016"
-  );
+  const currentUserId = "9e272678-02e9-445c-a77a-82122fada7da";
+
+  const fetchedDate = await chatFeature.service.getFetchedDate(currentUserId);
+  
   const messages = await getFetchedMessages(fetchedDate);
+
   return (
     <main className="container mx-auto p-2 rounded-md bg-base-200 mt-2 h-[85lvh] prose">
       <MessageBoard messages={messages} />
