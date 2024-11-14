8da14151084734c97aad75ed1413609a4c2c8bad
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-11-14 10:14:23 +0100

    add responive design
    Co-authored-by: Mårten Söderlind <marten.soderlind@appliedtechnology.se>
    Co-authored-by: Love <love.bellinder@gmail.com>
    Co-authored-by: Ali <ali.mohseni05@yahoo.se>

--- src/features/chat/components/form.tsx
+++ src/features/chat/components/form.tsx
@@ -3,7 +3,10 @@ import { addMessage } from "@/src/features/chat/actions";
 export default function Form() {
   return (
     <div>
-      <form className="flex flex-col items-center justify-center" action={addMessage}>
+      <form
+        className="flex flex-col items-center justify-center md:flex-row md:justify-end md:mt-6"
+        action={addMessage}
+      >
         <input
           className="input input-bordered input-accent w-full m-3"
           placeholder="type here"
@@ -11,7 +14,10 @@ export default function Form() {
           id="content"
           name="content"
         />
-        <button className="btn btn-accent m-1 w-full mb-4" type="submit">
+        <button
+          className="btn btn-accent m-1 w-full mb-4 md:mb-0 md:w-4/12"
+          type="submit"
+        >
           Send message
         </button>
       </form>
--- src/features/chat/components/token-count.tsx
+++ src/features/chat/components/token-count.tsx
@@ -4,13 +4,13 @@ import { revalidateMessages } from "@/src/features/chat/actions";
 export default function TokenCount({ totalTokens }: { totalTokens: number }) {
   return (
     <>
-      <div className="flex flex-col items-center justify-center">
-        <div id="counter" className="mb-1">
+      <div className="flex flex-col items-center justify-center md:flex-row md:justify-end md:mt-6">
+        <div id="counter" className="mb-1 md:mr-3">
           Remaining token: {totalTokens}
         </div>
         <button
           onClick={() => revalidateMessages()}
-          className="btn btn-accent m-1 w-full"
+          className="btn btn-accent m-1 w-full md:w-4/12"
         >
           Fetch messages
         </button>
