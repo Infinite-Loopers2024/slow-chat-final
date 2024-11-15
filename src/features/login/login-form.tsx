8fea0597e672ad549a4ccc951fa09d68e5aba99d
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-15 08:33:18 +0100

    login styling
    Co-authored-by: Ali <ali.mohseni05@yahoo.se>
    Co-authored-by: Love <love.bellinder@gmail.com>
    Co-authored-by: Ali <ali.mohseni05@yahoo.se>

--- src/features/login/login-form.tsx
+++ src/features/login/login-form.tsx
@@ -1,15 +1,19 @@
+import Link from "next/link";
+
 export default function LoginForm() {
   return (
     <form
       action="submit"
-      className="container mx-auto my-52 flex flex-col bg-base-200 p-3 rounded-md"
+      className="container mx-auto my-52 flex flex-col bg-base-300 p-3 rounded-md w-3/6"
     >
       <label htmlFor="user-name">Username</label>
       <input type="text" name="" id="user-name" required className="input" />
       <label htmlFor="password">Password</label>
       <input type="text" name="" id="password" required className="input" />
       <div>
-        <button className="btn my-2 bg-base-300">Join</button>
+        <button className="btn my-2 px-6 bg-base-300">
+          <Link href="/chat">Join</Link>
+        </button>
       </div>
     </form>
   );
