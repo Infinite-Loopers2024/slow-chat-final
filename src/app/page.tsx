7dd5354c4832712a97bdc0a7127947860e48fb6f
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-15 08:52:12 +0100

    login on frontpage
    Co-authored-by: Ali <ali.mohseni05@yahoo.se>
    Co-authored-by: Love <love.bellinder@gmail.com>
    Co-authored-by: Ali <ali.mohseni05@yahoo.se>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Link from "next/link";
+import { LoginForm } from "@/src/features/";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Link href="/chat">chat</Link>
+    <div className=" items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <LoginForm />
     </div>
   );
 }
--- src/features/login/login-form.tsx
+++ src/features/login/login-form.tsx
@@ -1,17 +1,17 @@
 import Link from "next/link";
 
-export default function LoginForm() {
+export function LoginForm() {
   return (
     <form
       action="submit"
-      className="container mx-auto my-52 flex flex-col bg-base-300 p-3 rounded-md w-3/6"
+      className="container mx-auto my-52 flex flex-col bg-base-300 p-3 rounded-md w-3/6 shadow-md"
     >
       <label htmlFor="user-name">Username</label>
-      <input type="text" name="" id="user-name" required className="input" />
+      <input type="text" name="" id="user-name" className="input" />
       <label htmlFor="password">Password</label>
-      <input type="text" name="" id="password" required className="input" />
+      <input type="text" name="" id="password" className="input" />
       <div>
-        <button className="btn my-2 px-6 bg-base-300">
+        <button className="btn btn-accent my-2 px-6">
           <Link href="/chat">Join</Link>
         </button>
       </div>
