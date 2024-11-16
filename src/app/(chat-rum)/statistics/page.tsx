5ee27114366fd70aa0fb219174e12b51b9fa4e89
defycab <love.bellinder@gmail.com>
2024-11-16 21:18:38 +0100

    remove fragments and slice date to just date
    Co-authored-by: Mårten Söderlind <marten.soderlind@appliedtechnology.se>
    Co-authored-by: Love <ali.mohseni05@yahoo.se>
    Co-authored-by: Adrienn <apozsgai97@gmail.com>

--- src/app/(chat-rum)/statistics/page.tsx
+++ src/app/(chat-rum)/statistics/page.tsx
@@ -2,11 +2,9 @@ import { Stats } from "@/src/features/chat/components/stats";
 
 export default function StatisticsPage() {
   return (
-    <>
-      <section>
-        <h1 className="text-center ">Stats</h1>
-        <Stats />
-      </section>
-    </>
+    <main>
+      <h1 className="text-center ">Stats</h1>
+      <Stats />
+    </main>
   );
 }
--- src/features/chat/components/stats.tsx
+++ src/features/chat/components/stats.tsx
@@ -5,44 +5,42 @@ export async function Stats() {
   const messageCount = await chatFeature.service.getAllMessagePerFetch();
   console.log(messages);
   return (
-    <>
-      <div className="flex flex-col items-center justify-center md:flex-row md:items-start">
-        <div className="w-8/12 md:w-4/12 flex flex-col items-center justify-center">
-          <h1 className="mt-8 text-center">Message count</h1>
-          <div className="stats stats-vertical shadow  mb-14 mt-4">
-            {messages.map((message, index) => (
-              <div className="stat shadow place-items-center" key={index}>
-                <div className="stat-title">User name</div>{" "}
-                <div className="stat-value text-sm">{message.userName}</div>
-                <div className="stat-title">Message count</div>{" "}
-                <div className="stat-value text-sm">{message.count}</div>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="w-8/12 md:w-4/12 flex flex-col items-center justify-center">
-          <h1 className="mb-4 text-center">Fetch count</h1>
-
-          <div className="stats stats-vertical shadow ">
-            {messageCount.map((message, index) => (
-              <div key={index}>
-                <div className="stat shadow place-items-center" key={index}>
-                  <div className="stat-title">Fetch date</div>{" "}
-                  <div className="stat-value text-sm">
-                    {message.fetchDate.timestamp}
-                  </div>
-                  <div className="stat-title">Fetch count</div>{" "}
-                  <div className="stat-value text-sm">{message.fetchCount}</div>
-                  <div className="stat-title">Messages on cooldown</div>{" "}
-                  <div className="stat-value text-sm">
-                    {message.messagesOnCooldown}
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
+    <div className="flex flex-col items-center justify-center md:flex-row md:items-start">
+      <div className="w-8/12 md:w-4/12 flex flex-col items-center justify-center">
+        <h1 className="mt-8 text-center">Message count</h1>
+        <div className="stats stats-vertical shadow  mb-14 mt-4">
+          {messages.map((message, index) => (
+            <div className="stat shadow place-items-center" key={index}>
+              <div className="stat-title">User name</div>{" "}
+              <div className="stat-value text-sm">{message.userName}</div>
+              <div className="stat-title">Message count</div>{" "}
+              <div className="stat-value text-sm">{message.count}</div>
+            </div>
+          ))}
         </div>
       </div>
-    </>
+      <div className="w-8/12 md:w-4/12 flex flex-col items-center justify-center">
+        <h1 className="mb-4 text-center">Fetch count</h1>
+
+        <div className="stats stats-vertical shadow ">
+          {messageCount.map((message, index) => (
+            <div key={index}>
+              <div className="stat shadow place-items-center" key={index}>
+                <div className="stat-title">Fetch date</div>{" "}
+                <div className="stat-value text-sm">
+                  {message.fetchDate.timestamp.slice(0, 11)}
+                </div>
+                <div className="stat-title">Fetch count</div>{" "}
+                <div className="stat-value text-sm">{message.fetchCount}</div>
+                <div className="stat-title">Messages on cooldown</div>{" "}
+                <div className="stat-value text-sm">
+                  {message.messagesOnCooldown}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
