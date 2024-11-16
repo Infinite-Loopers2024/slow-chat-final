e5d8a61cee785c51dcf598ba7ced6d6d360fdc12
defycab <love.bellinder@gmail.com>
2024-11-16 21:25:50 +0100

    div removal and minor styling stats header
    Co-authored-by: Mårten Söderlind <marten.soderlind@appliedtechnology.se>
    Co-authored-by: Love <ali.mohseni05@yahoo.se>
    Co-authored-by: Adrienn <apozsgai97@gmail.com>

--- src/features/chat/components/stats.tsx
+++ src/features/chat/components/stats.tsx
@@ -3,10 +3,9 @@ import { chatFeature } from "../instance";
 export async function Stats() {
   const messages = await chatFeature.service.getAllUserMessageById();
   const messageCount = await chatFeature.service.getAllMessagePerFetch();
-  console.log(messages);
   return (
     <div className="flex flex-col items-center justify-center md:flex-row md:items-start">
-      <div className="w-8/12 md:w-4/12 flex flex-col items-center justify-center">
+      <section className="w-8/12 md:w-4/12 flex flex-col items-center justify-center">
         <h1 className="mt-8 text-center">Message count</h1>
         <div className="stats stats-vertical shadow  mb-14 mt-4">
           {messages.map((message, index) => (
@@ -18,8 +17,9 @@ export async function Stats() {
             </div>
           ))}
         </div>
-      </div>
-      <div className="w-8/12 md:w-4/12 flex flex-col items-center justify-center">
+      </section>
+
+      <section className="w-8/12 md:w-4/12 flex flex-col items-center justify-center">
         <h1 className="mb-4 text-center">Fetch count</h1>
 
         <div className="stats stats-vertical shadow ">
@@ -40,7 +40,7 @@ export async function Stats() {
             </div>
           ))}
         </div>
-      </div>
+      </section>
     </div>
   );
 }
