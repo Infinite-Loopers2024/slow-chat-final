be5bf7cefd600d4e37bfeb8bb75a04ba92dcecac
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-18 10:46:45 +0100

    invalid password form
    Co-authored-by: Ali <ali.mohseni05@yahoo.se>
    Co-authored-by: Love <love.bellinder@gmail.com>
    Co-authored-by: Ali <ali.mohseni05@yahoo.se>

--- src/app/design-system/forms/page.tsx
+++ src/app/design-system/forms/page.tsx
@@ -9,6 +9,17 @@ export default function Form() {
         className="input input-border input-primary w-full max-w-xs m-3 rounded"
         placeholder="type here"
       />
+      <div className=" flex flex-col">
+        <input
+          className="input input-border focus:border-pink-300 focus:outline-pink-500 w-full max-w-xs m-3 rounded border-pink-500 text-pink-600 "
+          placeholder="Invalid password"
+          id="password"
+          name="password"
+        />
+        <p className="mt-2 text-pink-600 text-sm pl-4">
+          Please provide a valid password
+        </p>
+      </div>
     </>
   );
 }
