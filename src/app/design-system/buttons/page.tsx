c9b6271bf9984c2e4c051da20be68b5f64ef2873
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-11-18 09:58:20 +0100

    add differetn type of buttons
    Co-authored-by: Mårten Söderlind <marten.soderlind@appliedtechnology.se>
    Co-authored-by: Love <love.bellinder@gmail.com>
    Co-authored-by: Ali <ali.mohseni05@yahoo.se>

--- src/app/design-system/buttons/page.tsx
+++ src/app/design-system/buttons/page.tsx
@@ -1,3 +1,18 @@
 export default function Page() {
-  return <div>buttons</div>;
+  return (
+    <div className="container">
+      <button className="btn btn-accent rounded-lg m-1 w-full mb-4 md:mb-0 md:w-4/12">
+        Primary
+      </button>
+      <button className="btn btn-secondary rounded-md m-1 w-full mb-4 md:mb-0 md:w-4/12">
+        Secondary
+      </button>
+      <button className="btn btn-primary rounded-lg m-1 w-full mb-4 md:mb-0 md:w-4/12">
+        Accent
+      </button>
+      <button className="btn btn-disabled rounded-md m-1 w-full mb-4 md:mb-0 md:w-4/12">
+        Disabled
+      </button>
+    </div>
+  );
 }
