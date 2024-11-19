852b38f33c19ce043aaa1d822b00d725c14862d3
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-19 11:34:05 +0100

    renamed import
    Co-authored-by: Ali <ali.mohseni05@yahoo.se>
    Co-authored-by: Love <love.bellinder@gmail.com>
    Co-authored-by: Ali <ali.mohseni05@yahoo.se>

--- src/app/(chat-rum)/chat/page.tsx
+++ src/app/(chat-rum)/chat/page.tsx
@@ -1,9 +1,5 @@
 import { ChatPage } from "@/src/features";
 
 export default function Page() {
-  return (
-    <>
-      <ChatPage />
-    </>
-  );
+  return <ChatPage />;
 }
--- src/features/chat/components/message-board.tsx
+++ src/features/chat/components/message-board.tsx
@@ -1,13 +1,13 @@
 import { chatFeature } from "../instance";
 import type { Message } from "../type";
-import { Message as Msg } from "./message";
+import { Message as MessageComponent } from "./message";
 import TokenCount from "./token-count";
 
-export default async function MessageBoard({
-  messages,
-}: {
+type Props = {
   messages: Message[];
-}) {
+};
+
+export default async function MessageBoard({ messages }: Props) {
   const currentUserName = "Peter";
   const userId = "550e8400-e29b-41d4-a716-446655440000";
   const totalTokens = await chatFeature.service.getTokens(userId);
@@ -18,7 +18,7 @@ export default async function MessageBoard({
         className="h-4/6 bg-base-100 rounded-md m-2 overflow-auto flex flex-col"
       >
         {messages.map((message, index) => (
-          <Msg
+          <MessageComponent
             key={index}
             message={message}
             isCurrentUser={message.userName === currentUserName}
